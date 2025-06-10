e6f2f7f1545ae7e8d368fbea1aeecde670ce20c9
Schandroid243 <schadrackngunza@gmail.com>
2025-06-10 16:33:51 +0100

    feat(index.js): Added a boolean function in order to filter the we got from the stream we created and retrieve only the array of habitable planets and then console.log the names of those planets

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const { error } = require("console");
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+let habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanets(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(`${habitablePlanets.length} habitable planets found !`);
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
     console.log("We are don processing our file !");
   });
